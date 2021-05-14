7d34c3176075c2a60f051f6dfd18bee7396a7de9
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-05-14 13:07:14 +0200

    finish step3

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567' },
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (e) => {
     e.preventDefault()
@@ -15,10 +18,12 @@ const App = () => {
 
     const personObj = {
       name: newName,
+      number: newNumber,
     }
 
     setPersons((persons) => persons.concat(personObj))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -30,12 +35,21 @@ const App = () => {
           <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
+          number:{' '}
+          <input
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person, index) => (
-        <div key={person.name + index}>{person.name}</div>
+        <div key={person.name + index}>
+          {person.name} {person.number}
+        </div>
       ))}
     </div>
   )
