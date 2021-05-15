d1384ecc6708bbc606e9fec3f7bde9752c0063d6
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-05-15 14:44:06 +0200

    finish step8

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,7 +28,21 @@ const generateId = () => {
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+app.use(
+  morgan((tokens, req, res) =>
+    [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
+      JSON.stringify(req.body),
+    ].join(' ')
+  )
+)
 
 app.get('/info', (_req, res) =>
   res.send(
